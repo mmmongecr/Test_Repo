536bc7c492f48b044728ded7ea2aad2ec62ade39
Manuel Mora M. <mmmongecr@hotmail.com>
2024-02-01 12:12:58 -0600

    agregar archivo New.java

---
+++ New.java
@@ -0,0 +1,25 @@
+/*PRUEBA*/
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.parqueos.del.oeste;
+
+import ui.F_Login;
+import ui.F_MainWindow;
+
+/**
+ *
+ * @author manuel.mora
+ */
+public class ProyectoParqueosDelOeste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        F_Login log = new F_Login(); // Inicializa la ventana de login
+        
+    }
+}
